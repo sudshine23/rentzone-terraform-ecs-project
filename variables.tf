6996c73e25aa48c73d1bbe1fd59bc26d4d02e07b
Sudarshan <jhengis@protonmail.com>
2023-06-13 10:58:46 +0530

    creted vpc file

--- variables.tf
+++ variables.tf
@@ -16,3 +16,39 @@ variable "environment" {
   type = string
   
 }
+
+#VPC variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type = string
+  }
+
+variable "public_subnet_az1_cidr" {
+  description = "public subnet AZ1 cider block"
+  type = string
+  }
+
+variable "public_subnet_az2_cidr" {
+  description = "public subnet AZ2 cider block"
+  type = string
+  }
+
+variable "private_app_subnet_az1_cidr" {
+  description = "private app subnet AZ1 cider block"
+  type = string
+  }
+
+variable "private_app_subnet_az2_cidr" {
+  description = "private app subnet AZ2 cider block"
+  type = string
+  }
+
+variable "private_data_subnet_az1_cidr" {
+  description = "private data subnet AZ1 cider block"
+  type = string
+  }
+
+variable "private_data_subnet_az2_cidr" {
+  description = "private data subnet AZ2 cider block"
+  type = string
+  }
