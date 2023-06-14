6dffb2e833d46c54979c7f8699988386d0537ab2
Sudarshan <jhengis@protonmail.com>
2023-06-14 09:40:39 +0530

    Created alb, rds, SG, acm

--- variables.tf
+++ variables.tf
@@ -52,3 +52,45 @@ variable "private_data_subnet_az2_cidr" {
   description = "private data subnet AZ2 cider block"
   type = string
   }
+
+# Security Groups variable
+variable "ssh_location" {
+  description = "IP address that can ssh into the server"
+  type = string
+  }
+
+  # RDS variables
+
+variable "database_snapshot_identifier" {
+  description = "database snapshot name"
+  type = string
+  }
+
+variable "database_instance_class" {
+  description = "database instance type"
+  type = string
+  }
+
+variable "database_instance_identifier" {
+  description = "database instance identifier"
+  type = string
+  }
+
+variable "multi_az_deployement" {
+  description = "create standby DB instance"
+  type = bool
+  }
+
+
+#acm variables
+variable "domain_name" {
+  description = "domain name"
+  type = string
+  }
+
+
+variable "alternative_names" {
+  description = "sub domain name"
+  type = string
+  }
+  
